b5f07f2969470d1255452fd80e42b4175bf80f27
tommyyxou <tommy.yx.ou@gmail.com>
2019-03-01 21:57:18 -0800

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,281 @@
 let triviaQuestions = [
-{
-    question:"question 1",
-    answer1:"q1ans1",
-    answer2:"q1ans2",
-    answer3:"q1ans3",
-    answer4:"q1ans4",
-    correct:"q1ans2",
-    img:"",
-},
-{
-    question:"question 2",
-    answer1:"q2ans1",
-    answer2:"q2ans2",
-    answer3:"q2ans3",
-    answer4:"q2ans4",
-    correct:"q2ans1",
-    img:"",
-},
-{
-    question:"question 3",
-    answer1:"q3ans1",
-    answer2:"q3ans2",
-    answer3:"q3ans3",
-    answer4:"q3ans4",
-    correct:"q3ans3",
-    img:"",
-},
-{
-    question:"question 4",
-    answer1:"q4ans1",
-    answer2:"q4ans2",
-    answer3:"q4ans3",
-    answer4:"q4ans4",
-    correct:"q4ans3",
-    img:"",
-},
-{
-    question:"question 5",
-    answer1:"q5ans1",
-    answer2:"q5ans2",
-    answer3:"q5ans3",
-    answer4:"q5ans4",
-    correct:"q5ans3",
-    img:"",
-},
+    [
+        {
+            question:"The nuts that hold the rim onto the car are called?",
+            answer1:"Wheel nuts",
+            answer2:"Nuts",
+            answer3:"Lug nuts",
+            answer4:"Wheel Fasteners",
+            correct:"Lug nuts",
+            img:"./assets/images/e1.jpg",
+        },
+        {
+            question:"Lock-in hubs are used to engage?",
+            answer1:"Limited slip differential",
+            answer2:"Posi-trac",
+            answer3:"Four wheel drive",
+            answer4:"Theft protection",
+            correct:"Four wheel drive",
+            img:"./assets/images/e2.jpg",
+        },
+        {
+            question:"Three on the tree refers to the shifting gear being located on what part of the vehicle?",
+            answer1:"Center Console",
+            answer2:"Steering column",
+            answer3:"Steering wheel",
+            answer4:"Floor",
+            correct:"Steering column",
+            img:"./assets/images/e3.jpg",
+        },
+        {
+            question:"M+S combination tires are intended for?",
+            answer1:"Off roading",
+            answer2:"Mud and snow use",
+            answer3:"Messy and slippery situation use",
+            answer4:"General use",
+            correct:"Mud and snow use",
+            img:"./assets/images/e4.jpg",
+        },
+        {
+            question:"The headers are part of which vehicle system?",
+            answer1:"Exhaust",
+            answer2:"Drive train",
+            answer3:"Seating",
+            answer4:"Wheel assembly",
+            correct:"Exhaust",
+            img:"./assets/images/e5.jpg",
+        },
+        {
+            question:"The distributor cap controls what function?",
+            answer1:"Lubrication distribution",
+            answer2:"Cooling distribution",
+            answer3:"Spark distribution",
+            answer4:"Power distribution",
+            correct:"Spark distribution",
+            img:"./assets/images/e6.jpg",
+        },
+        {
+            question:"Coupe vehicles have how many doors?",
+            answer1:"Three",
+            answer2:"Four",
+            answer3:"Two",
+            answer4:"One",
+            correct:"Two",
+            img:"./assets/images/e7.jpg",
+        },
+        {
+            question:"Spoilers are used to perform what function?",
+            answer1:"Increase fuel economy",
+            answer2:"Look cool",
+            answer3:"Add down force",
+            answer4:"Add up force",
+            correct:"Add down force",
+            img:"./assets/images/e8.jpg",
+        },
+        {
+            question:"Ram air forces air into what part of the vehicle?",
+            answer1:"Into the Cab",
+            answer2:"Into the engine compartment",
+            answer3:"Over the Car",
+            answer4:"Into the air conditioning unit",
+            correct:"Into the engine compartment",
+            img:"./assets/images/e9.jpg",
+        },
+        {
+            question:"Halo headlights are controversial because of their?",
+            answer1:"Brightness",
+            answer2:"Color",
+            answer3:"Shape",
+            answer4:"Fragile nature",
+            correct:"Brightness",
+            img:"./assets/images/e10.jpg",
+        },
+    ],
+    [
+        {
+            question:"Who designed what was considered the world’s first car?",
+            answer1:"Henry Ford",
+            answer2:"Karl Benz",
+            answer3:"Louis Chevrolet",
+            answer4:"Ferdinand Porsche",
+            correct:"Karl Benz",
+            img:"./assets/images/n1.jpg",
+        },
+        {
+            question:"What famous auto maker is responsible for designing the Volkswagen Beetle?",
+            answer1:"Ferdinand Porsche",
+            answer2:"Bavarian Motor Works",
+            answer3:"Henry Ford",
+            answer4:"Walter P. Chrysler",
+            correct:"Ferdinand Porsche",
+            img:"./assets/images/n2.jpg",
+        },
+        {
+            question:"What make and model is the General Lee?",
+            answer1:"Dodge Charger",
+            answer2:"Chevrolet Camaro",
+            answer3:"Ford Mustang",
+            answer4:"Mercury Cougar",
+            correct:"Dodge Charger",
+            img:"./assets/images/n3.jpg",
+        },
+        {
+            question:"How did the Ford Mustang get it’s name?",
+            answer1:"Herd of Wild Horses",
+            answer2:"WWII Fighter Plane",
+            answer3:"Henry Ford’s Wife",
+            answer4:"Play on words for Horsepower ",
+            correct:"WWII Fighter Plane",
+            img:"./assets/images/n4.jpg",
+        },
+        {
+            question:"Which car is considered to be the first “muscle car”?",
+            answer1:"1963 Buick Rivera",
+            answer2:"1962 Chevrolet Imala",
+            answer3:"1964 Pontiac GTO",
+            answer4:"1967 Chevrolet Camaro",
+            correct:"1964 Pontiac GTO",
+            img:"./assets/images/n5.jpg",
+        },
+        {
+            question:"What is the worlds largest automotive company?",
+            answer1:"Volkswagen Group",
+            answer2:"General Motors",
+            answer3:"Toyota Motor",
+            answer4:"Ford Motors",
+            correct:"Toyota Motor",
+            img:"./assets/images/n6.jpg",
+        },
+        {
+            question:"What company opened up the first drive-in gas station?",
+            answer1:"Mobile",
+            answer2:"Exxon",
+            answer3:"Shell",
+            answer4:"Gulf",
+            correct:"Gulf",
+            img:"./assets/images/n7.jpg",
+        },
+        {
+            question:"When and where was the first drive-in theater built?",
+            answer1:"Detroit Michigan in 1928",
+            answer2:"NYC, New York in 1930",
+            answer3:"Camen, NJ in 1933",
+            answer4:"Philadelphia, PA in 1929",
+            correct:"Camen, NJ in 1933",
+            img:"./assets/images/n8.jpg",
+        },
+        {
+            question:"What US Production car is the fastest from 0-60 mph?",
+            answer1:"1962 Chevrolet Impala SS 409",
+            answer2:"1966 Ford Shelby GT 350",
+            answer3:"1968 Chevrolet Camaro RS 327",
+            answer4:"1966 Dodge Charger",
+            correct:"1962 Chevrolet Impala SS 409",
+            img:"./assets/images/n9.jpg",
+        },
+        {
+            question:"What is the name of the caped lady on top of the Rolls Royce radiators?",
+            answer1:"Flying Woman",
+            answer2:"Queen Elizabeth",
+            answer3:"Spirit of Ecstasy",
+            answer4:"Lady Liberty",
+            correct:"Spirit of Ecstasy",
+            img:"./assets/images/n10.jpg",
+        },
+    ],
+    [
+        {
+            question:"In what year did Auto Insurance become available to drivers?",
+            answer1:"1897",
+            answer2:"1890",
+            answer3:"1902",
+            answer4:"1918",
+            correct:"1897",
+            img:"./assets/images/h1.png",
+        },
+        {
+            question:"Harley Earl introduces what feature to the Chevy Cadillac in 1948",
+            answer1:"Wheel Wells",
+            answer2:"Radio",
+            answer3:"Blinkers",
+            answer4:"Tail Fins",
+            correct:"Tail Fins",
+            img:"./assets/images/h2.jpg",
+        },
+        {
+            question:"Why was a national speed limit of 55 mph enacted in 1974?",
+            answer1:"Increase in automobile accidents",
+            answer2:"Increase in speeding tickets",
+            answer3:"Oil Shortage",
+            answer4:"Increase in city traffic",
+            correct:"Oil Shortage",
+            img:"./assets/images/h3.jpg",
+        },
+        {
+            question:"When did Chrysler first officially use the 'Hemi' name?",
+            answer1:"1964",
+            answer2:"1954",
+            answer3:"1965",
+            answer4:"1960",
+            correct:"1964",
+            img:"./assets/images/h4.jpg",
+        },
+        {
+            question:"What was the first Japanese car to be produced in the United States?",
+            answer1:"Toyota Camry",
+            answer2:"Honda Civic",
+            answer3:"Nissan Altima",
+            answer4:"Honda Accord",
+            correct:"Honda Accord",
+            img:"./assets/images/h5.jpg",
+        },
+        {
+            question:"What was Henry Ford most famous for?",
+            answer1:"Inventing the Automobile",
+            answer2:"Inventing Combustion Engines",
+            answer3:"Development of the assembly line used in mass production",
+            answer4:"Development of the Ford Mustang",
+            correct:"Development of the assembly line used in mass production",
+            img:"./assets/images/h6.jpg",
+        },
+        {
+            question:"What video game comes standard in every Saab equipped with a GM Tech II?",
+            answer1:"Tetris",
+            answer2:"Mario Cart",
+            answer3:"Centipede",
+            answer4:"Pong",
+            correct:"Pong",
+            img:"./assets/images/h7.jpg",
+        },
+        {
+            question:"What tailgate accessory did the first and second generation Honda CRV’s come equipped with?",
+            answer1:"Beverage Cooler",
+            answer2:"Tailgate Grill",
+            answer3:"Picnic Table",
+            answer4:"Porta-Potty",
+            correct:"Picnic Table",
+            img:"./assets/images/h8.jpg",
+        },
+        {
+            question:"What was the last automaker to offer cassette players in their vehicles?",
+            answer1:"Ford",
+            answer2:"Lexus",
+            answer3:"Honda",
+            answer4:"Chevrolet",
+            correct:"Ford",
+            img:"./assets/images/h9.jpg",
+        },
+        {
+            question:"Bentley, Bugatti, Lamborghini and Porsche are auto manufacturers under what flagship company?",
+            answer1:"BMW",
+            answer2:"Jaguar",
+            answer3:"Volkswagen",
+            answer4:"Subaru",
+            correct:"Volkswagen",
+            img:"./assets/images/h10.jpg",
+        },
+    ],
 ];
-
 let questionNumber = 0;
 let question = "";
 let ans1 = "";
@@ -57,8 +287,54 @@ let timerId = null;
 let numOfCorrect = 0;
 let numOfWrong = 0;
 let numOfTimeout = 0;
+let array = null;
+let difficultynumber = 0;
+let timerSettingSpan = 30;
+
+function initiateDifTimer () {
+    $(".difficulty").click(selectDifficulty);
+    $(".timerSetting").click(selectTimer);
+}
+
+function selectDifficulty (e) {
+    let difficultyLevel = e.currentTarget.innerText
+
+    switch (difficultyLevel) {
+        case "Easy":
+            difficultynumber = 0;
+            break;
+        case "Normal":
+            difficultynumber = 1;
+            break;
+        case "Hard":
+            difficultynumber = 2;
+            break
+    };
+    $("#difficultySpan").text(difficultyLevel);
+};
+
+function selectTimer (e) {
+    let timerSpan = e.currentTarget.innerText
+
+    switch (timerSpan) {
+        case "10":
+            timerSettingSpan = 10;
+            countDown = timerSettingSpan;
+            break;
+        case "20":
+            timerSettingSpan = 20;
+            countDown = timerSettingSpan;
+            break;
+        case "30":
+            timerSettingSpan = 30;
+            countDown = timerSettingSpan;
+            break
+    };
+    $("#timerSettingSpan").text(timerSpan);
+};
 
 function loadQuestion (array) {
+
     question = $("<div>");
     question.attr ('class', "question");
     question.html (array.question);
@@ -98,29 +374,32 @@ function countDownTimer () {
 
 function timeout () {
     timerQuestionReset();
-    $("#message").append("Time Out! The Correct Answer is:", triviaQuestions[questionNumber].correct);
+    $("#message").append("Time Out! The Correct Answer is:", array.correct);
+    $("#image").append("<img src='" + triviaQuestions[difficultynumber][questionNumber].img + "'></img>");
     setTimeout("nextQuestion()", 5000);
-    countDown = 30;
+    countDown = timerSettingSpan;
     numOfTimeout++;
 };
 
 function checkAnswer(e) {
     let clickedDiv = e.currentTarget;
 
-    if (clickedDiv.innerText === triviaQuestions[questionNumber].correct) {
+    if (clickedDiv.innerText === array.correct) {
         timerQuestionReset();
         $("#message").append("Correct!");
+        $("#image").append("<img src='" + triviaQuestions[difficultynumber][questionNumber].img + "'></img>");
         clearInterval(timerId);
         setTimeout("nextQuestion()", 5000);
-        countDown = 30;
+        countDown = timerSettingSpan;
         numOfCorrect++;
 
     } else {
         timerQuestionReset();
-        $("#message").append("Wrong! The Correct Answer is:", triviaQuestions[questionNumber].correct);
+        $("#message").append("Wrong! The Correct Answer is:", array.correct);
+        $("#image").append("<img src='" + triviaQuestions[difficultynumber][questionNumber].img + "'></img>");
         clearInterval(timerId);
         setTimeout("nextQuestion()", 5000);
-        countDown = 30;
+        countDown = timerSettingSpan;
         numOfWrong++;
     };
 };
@@ -128,12 +407,13 @@ function checkAnswer(e) {
 function nextQuestion () {
     $("#message").remove();
     $("#messageDiv").append("<div id='message'></div>");
-
-    if (questionNumber === (triviaQuestions.length-1)) {
+    $("#image").remove();
+    $("#imageDiv").append("<div id='image'></div>");
+    if (questionNumber === (triviaQuestions[difficultynumber].length-1)) {
         gameEnd ();
     } else {
     questionNumber++;
-    let array = triviaQuestions[questionNumber];
+    array = triviaQuestions[difficultynumber][questionNumber];
     loadQuestion (array);
     };
 };
@@ -147,16 +427,18 @@ function appendQuestion (display) {
 };
 
 function start () {
-    let array = triviaQuestions[questionNumber]
+    array = triviaQuestions[difficultynumber][questionNumber];
     loadQuestion (array);
     $("#start").remove();
+    $("#difficulty").remove();
+    $("#timerSetting").remove();
 };
 
 function gameEnd () {
     $("#message").append("<div class='final'>Number of Correct: " + numOfCorrect + "</div>");
     $("#message").append("<div class='final'>Number of Wrong: " + numOfWrong + "</div>");
     $("#message").append("<div class='final'>Number of Timeout: " + numOfTimeout + "</div>");
-    $("#restartDiv").append("<button id='restart' onClick='reset();'>Restart</button>");
+    $("#restartDiv").append("<div class='child button2' id='restart' onClick='reset();'>Restart</div>");
 };
 
 function timerQuestionReset () {
@@ -174,6 +456,21 @@ function reset () {
     numOfCorrect = 0;
     numOfWrong = 0;
     numOfTimeout = 0;
-    let array = triviaQuestions[questionNumber];
-    loadQuestion (array);
+    difficultynumber = 0;
+    timerSettingSpan = 30;
+    
+    $("#startDiv").append("<div class='child button2' id='start' onclick='start();'>Start</div>")
+    
+    $("#difficultyDiv").append("<div id='difficulty'></div>");
+    $("#difficulty").append("<div class='difficulty button1'>Easy</div>");
+    $("#difficulty").append("<div class='difficulty button1'>Normal</div>");
+    $("#difficulty").append("<div class='difficulty button1'>Hard</div>");
+    $("#difficulty").append("<div>Please Select Difficulty Level: <span id='difficultySpan'>Easy</span></div>");
+    
+    $("#timerSettingDiv").append("<div id='timerSetting'></div>");
+    $("#timerSetting").append("<div class='timerSetting button1'>10</div>");
+    $("#timerSetting").append("<div class='timerSetting button1'>20</div>");
+    $("#timerSetting").append("<div class='timerSetting button1'>30</div>");
+    $("#timerSetting").append("<div>Please Select Time Span For Each Questions: <span id='timerSettingSpan'>30</span> seconds</div>");
+    initiateDifTimer ();
 };
